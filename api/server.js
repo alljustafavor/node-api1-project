2394056401235be03d63eb3cddf4c87c637fe11c
Joseph Noblett <noblettjoe@gmail.com>
2023-12-06 19:03:21 -0600

    delete and update both work

--- api/server.js
+++ api/server.js
@@ -55,6 +55,49 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        const user = await User.update(id, updates)
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            });
+        } else {
+            if (!updates.name || !updates.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "The user information could not be retrieved"
+        });
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const delete_user = await User.remove(id);
+        if (!delete_user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            });
+        } else {
+            res.status(200).json(delete_user);
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        });
+    }
+})
+
 
 
 module.exports = server; 
